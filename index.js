6dacc9918cac11cf138b54b8ff777bb554f30551
Richard Torruellas <rtorruellas@gmail.com>
2015-05-13 09:43:25 -0700

    adding in globals for .Deffered

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
  * Released under the MIT license
  * http://jquery.org/license
  *
- * Date: 2015-05-06T23:19Z
+ * Date: 2015-05-13T16:31Z
  */
 
 (function( global, factory ) {
@@ -6242,7 +6242,33 @@ jQuery.fn.load = function( url, params, callback ) {
 
 
 
-jQuery.noConflict = function() {};
+
+var
+	// Map over jQuery in case of overwrite
+	_jQuery = window.jQuery,
+
+	// Map over the $ in case of overwrite
+	_$ = window.$;
+
+jQuery.noConflict = function( deep ) {
+	if ( window.$ === jQuery ) {
+		window.$ = _$;
+	}
+
+	if ( deep && window.jQuery === jQuery ) {
+		window.jQuery = _jQuery;
+	}
+
+	return jQuery;
+};
+
+// Expose jQuery and $ identifiers, even in AMD
+// (#7102#comment:10, https://github.com/jquery/jquery/pull/557)
+// and CommonJS for browser emulators (#13566)
+if ( typeof noGlobal === strundefined ) {
+	window.jQuery = window.$ = jQuery;
+}
+
 
 
 
